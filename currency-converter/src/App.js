0473d6c7dc2f0a49ba5774018d1082f66d7ad902
sylbaerchen <hi.silviabar@gmail.com>
2023-05-02 22:11:41 +0200

    ✨ add GET function

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -1,13 +1,25 @@
+import React, { useEffect, useState } from "react";
 import CurrencyRow from "./CurrencyRow";
-import './App.css';
+import "./App.css";
+
+const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
+console.log(ACCESS_KEY);
+
+const BASE_URL = `https://api.freecurrencyapi.com/v1/latest?apikey=${ACCESS_KEY}&base_currency=EUR`;
 
 function App() {
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div>
       <h1>Convert</h1>
       <CurrencyRow />
       <div className="equals">=</div>
-      <CurrencyRow/>
+      <CurrencyRow />
     </div>
   );
 }
