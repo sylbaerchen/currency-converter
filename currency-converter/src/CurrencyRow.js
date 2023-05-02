cd4e775f2c0bf487b022a7556f77aebbe1249a61
sylbaerchen <hi.silviabar@gmail.com>
2023-05-02 22:43:26 +0200

    ✨ add initial state currencies

--- currency-converter/src/CurrencyRow.js
+++ currency-converter/src/CurrencyRow.js
@@ -2,12 +2,13 @@ import React from "react";
 
 export default function CurrencyRow(props) {
   const {
-    currencyOptions
+    currencyOptions, 
+    selectedCurrency
   } = props
   return (
     <div>
       <input className='input' type="number"/>
-      <select>
+      <select value={selectedCurrency}>
         {currencyOptions.map(option => (
           <option key={option} value={option}>{option}</option>
         ))}
